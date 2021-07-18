25e871ea25ec8b1ff90a1c22df045d5b2dd0efdb
Shen-Ta Hsieh <beststeve@secondstate.io>
2021-07-19 02:17:57 +0800

    [AOT] Create custom section for aot binary format
    * Add test for custom section version
    * Changes in aot format:
      - Remove init function
      - Remove types/codes array
      - Add intrinsic variable
    
    Signed-off-by: Shen-Ta Hsieh <beststeve@secondstate.io>

--- include/common/astdef.h
+++ include/common/astdef.h
@@ -46,7 +46,8 @@ enum class ASTNodeAttr : uint8_t {
   Type_Table,
   Type_Global,
   Expression,
-  Instruction
+  Instruction,
+  Sec_AOT,
 };
 
 /// AST node attributes enumeration string mapping.
--- lib/loader/ldmgr.cpp
+++ lib/loader/ldmgr.cpp
@@ -11,13 +11,13 @@ Expect<void> LDMgr::setPath(const std::filesystem::path &FilePath) {
     return Unexpect(Res);
   }
 
-  const auto Table = getSymbol<void(const void *)>("init");
-  if (unlikely(!Table)) {
+  const auto IntrinsicsTable = getSymbol<const void *>("intrinsics");
+  if (unlikely(!IntrinsicsTable)) {
     spdlog::error(ErrCode::InvalidGrammar);
     return Unexpect(ErrCode::InvalidGrammar);
   }
 
-  Table(Intrinsics);
+  *IntrinsicsTable = Intrinsics;
   return {};
 }
 
